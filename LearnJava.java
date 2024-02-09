d3e5e921f88e098722a63054ac0d8171098c3ab8
Eng Omar <engomaralryan@gmail.com>
2024-02-09 22:36:40 +0200

    add the some solvings

--- LearnJava.java
+++ LearnJava.java
@@ -99,56 +99,131 @@ public class LearnJava {
          * };
          */
 
-        try (Scanner inputTicket = new Scanner(System.in)) {
-            int weightBag, priceOfIncWeight = 0, numOfPassengers, totalIncWeight, totalPrice = 0;
+        /*
+         * try (Scanner inputTicket = new Scanner(System.in)) {
+         * int weightBag, priceOfIncWeight = 0, numOfPassengers, totalIncWeight,
+         * totalPrice = 0;
+         * 
+         * System.out.println("Enter Numbers of passengers");
+         * numOfPassengers = inputTicket.nextInt();
+         * 
+         * for (int i = 1; i <= numOfPassengers; i++) {
+         * 
+         * System.out.println("Enter Type of ticket f|b|e");
+         * char typeOfTicket = inputTicket.next().charAt(0);
+         * switch (typeOfTicket) {
+         * case 'f':
+         * case 'F':
+         * System.out.println("Enter The weight of bag for passenger no. " + i);
+         * weightBag = inputTicket.nextInt();
+         * if (weightBag > 30) {
+         * priceOfIncWeight = (weightBag - 30) * 10;
+         * totalPrice += priceOfIncWeight;
+         * }
+         * System.out.println("The price of increment weight = " + priceOfIncWeight +
+         * " $");
+         * break;
+         * case 'b':
+         * case 'B':
+         * System.out.println("Enter The weight of bag for passenger no. " + i);
+         * weightBag = inputTicket.nextInt();
+         * if (weightBag > 25) {
+         * priceOfIncWeight = (weightBag - 25) * 10;
+         * totalPrice += priceOfIncWeight;
+         * }
+         * System.out.println("The price of increment weight = " + priceOfIncWeight +
+         * " $");
+         * break;
+         * case 'e':
+         * case 'E':
+         * System.out.println("Enter The weight of bag for passenger no. " + i);
+         * weightBag = inputTicket.nextInt();
+         * if (weightBag > 20) {
+         * priceOfIncWeight = (weightBag - 20) * 10;
+         * totalPrice += priceOfIncWeight;
+         * }
+         * System.out.println("The price of increment weight = " + priceOfIncWeight +
+         * " $");
+         * break;
+         * 
+         * default:
+         * System.out.println("the type ticket not found please check again");
+         * break;
+         * }
+         * }
+         * totalIncWeight = totalPrice / 10;
+         * System.out.println("the total increment weight A1 = " + totalIncWeight +
+         * " KG");
+         * System.out.println("the total price of all increment weight A1 = " +
+         * totalPrice + " $");
+         * }
+         */
 
-            System.out.println("Enter Numbers of passengers");
-            numOfPassengers = inputTicket.nextInt();
+        int arrNum[] = { 7, -11, 8, 4, -2, 5, 1, -9 };
+        int sum = 0, count = 0, average;
+        for (int i = 0; i < arrNum.length; i++) {
+            if (arrNum[i] > 0) {
+                sum += arrNum[i];
+                count++;
+            }
+        }
+        average = sum / count;
+        System.out.println("the sum = " + sum);
+        System.out.println("the average = " + average);
 
-            for (int i = 1; i <= numOfPassengers; i++) {
+        try (Scanner valueList = new Scanner(System.in)) {
 
-                System.out.println("Enter Type of ticket f|b|e");
-                char typeOfTicket = inputTicket.next().charAt(0);
-                switch (typeOfTicket) {
-                    case 'f':
-                    case 'F':
-                        System.out.println("Enter The weight of bag for passenger no. " + i);
-                        weightBag = inputTicket.nextInt();
-                        if (weightBag > 30) {
-                            priceOfIncWeight = (weightBag - 30) * 10;
-                            totalPrice += priceOfIncWeight;
-                        }
-                        System.out.println("The price of increment weight = " + priceOfIncWeight + " $");
-                        break;
-                    case 'b':
-                    case 'B':
-                        System.out.println("Enter The weight of bag for passenger no. " + i);
-                        weightBag = inputTicket.nextInt();
-                        if (weightBag > 25) {
-                            priceOfIncWeight = (weightBag - 25) * 10;
-                            totalPrice += priceOfIncWeight;
-                        }
-                        System.out.println("The price of increment weight = " + priceOfIncWeight + " $");
-                        break;
-                    case 'e':
-                    case 'E':
-                        System.out.println("Enter The weight of bag for passenger no. " + i);
-                        weightBag = inputTicket.nextInt();
-                        if (weightBag > 20) {
-                            priceOfIncWeight = (weightBag - 20) * 10;
-                            totalPrice += priceOfIncWeight;
-                        }
-                        System.out.println("The price of increment weight = " + priceOfIncWeight + " $");
-                        break;
+            int[][] arrNumbers = new int[5][3];
+            int sumArr = 0;
 
-                    default:
-                        System.out.println("the type ticket not found please check again");
-                        break;
+            // Input Numbers
+            // fillArray(arrNumbers)
+            // or
+            System.out.println("the Array is fill");
+            for (int i = 0; i < arrNumbers.length; i++) {
+                for (int j = 0; j < arrNumbers[0].length; j++) {
+                    System.out.println("Enter the Number :" + i + "=>" + j);
+                    arrNumbers[i][j] = valueList.nextInt();
                 }
             }
-            totalIncWeight = totalPrice / 10;
-            System.out.println("the total increment weight A1 = " + totalIncWeight + " KG");
-            System.out.println("the total price of all increment weight A1 = " + totalPrice + " $");
+            // print Numbers
+            // printArray(arrNumbers)
+            // or
+            System.out.println("the Array print");
+            for (int i = 0; i < arrNumbers.length; i++) {
+                for (int j = 0; j < arrNumbers[0].length; j++) {
+                    System.out.println(arrNumbers[i][j]);
+                }
+            }
+
+            // Sum Number from Array
+            for (int i = 0; i < arrNumbers.length; i++) {
+                for (int j = 0; j < arrNumbers[0].length; j++) {
+                    sumArr += arrNumbers[i][j];
+                }
+            }
+            System.out.println("the sum = " + sumArr);
         }
     }
+
 }
+// Used Class
+/*
+ * static Scanner valueList = new Scanner(System.in); // move top the main
+ * public static void fillArray(int[][] list) {
+ * for (int i = 0; i < list.length; i++) {
+ * for (int j = 0; j < list[0].length; j++) {
+ * System.out.println("Enter the Number :" + i + "=>" + j);
+ * list[i][j] = valueList.nextInt();
+ * }
+ * }
+ * }
+ * //
+ * public static void printArray(int[][] list) {
+ * for (int i = 0; i < list.length; i++) {
+ * for (int j = 0; j < list[0].length; j++) {
+ * System.out.println(list[i][j]);
+ * }
+ * }
+ * }
+ */
