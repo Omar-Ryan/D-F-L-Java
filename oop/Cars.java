930c4058f15524a31ed27a1912f85c97ffd6c9a1
Eng Omar <engomaralryan@gmail.com>
2024-02-12 21:58:32 +0200

    add the some oop examples

---
+++ oop/Cars.java
@@ -0,0 +1,24 @@
+package oop;
+
+public class Cars {
+    private String make;
+    private int yearsModel;
+
+    // Setter
+    public void setMake(String ma) {
+        make = ma;
+    }
+
+    public void setYearsModel(int yM) {
+        yearsModel = yM;
+    }
+
+    // getter
+    public String getMake() {
+        return make;
+    }
+
+    public int getYearsModel() {
+        return yearsModel;
+    }
+}
---
+++ oop/learnOop.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class learnOop {
+    public static void main(String[] args) {
+
+        Cars infoCars = new Cars();
+
+        infoCars.setMake("Egypt");
+        infoCars.setYearsModel(2017);
+        System.out.println(infoCars.getMake());
+        // System.out.println(infoCars.getYearsModel());
+    }
+}
